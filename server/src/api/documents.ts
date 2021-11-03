5f7e9e08456a58b52adb06c847934f4f4be4ca2b
suky <0xsuky@gmail.com>
2021-11-03 22:16:40 +0900

    feat: search api 생성
    - /documents/search api 생성
    - generation, boostcamp_id, name 확인
    - validator는 추후생성 예정

--- server/src/api/documents.ts
+++ server/src/api/documents.ts
@@ -1,10 +1,21 @@
 import * as express from 'express';
-import { getRecentUpdatedDoc } from '../sql/documents-query';
+import { getRecentUpdatedDoc, getSearchDoc } from '../sql/documents-query';
+import { DocumentsSearch } from '../types/apiInterface';
 
 const router = express.Router();
-router.get('/', async (req: express.Request, res:express.Response)=>{
-    let result = await getRecentUpdatedDoc()
-    res.json({'hi': 'boocam'});
-})
+router.get('/', async (req: express.Request, res: express.Response) => {
+  let result = await getRecentUpdatedDoc();
+  res.json({ hi: 'boocam' });
+});
+
+router.get('/search', async (req: express.Request, res: express.Response) => {
+  const queryParam: DocumentsSearch = req.query;
+  try {
+    const result = await getSearchDoc(queryParam);
+    res.status(200).json({ result, msg: 'success' });
+  } catch (err) {
+    res.status(404).json({ result: [], msg: 'fail' });
+  }
+});
 
 export default router;
--- server/src/sql/documents-query.ts
+++ server/src/sql/documents-query.ts
@@ -1,5 +1,19 @@
-import db from '../services/db-pool'
-export async function getRecentUpdatedDoc(){
-    let result =await db.pool.query('SELECT * FROM update;');
-    return result
+import db from '../services/db-pool';
+import { DocumentsSearch } from '../types/apiInterface';
+
+export async function getRecentUpdatedDoc() {
+  let result = await db.pool.query('SELECT * FROM update;');
+  return result;
+}
+
+export async function getSearchDoc(params: DocumentsSearch) {
+  const [result] = await db.pool.query(
+    'SELECT generation, boostcamp_id, name ' +
+      'FROM `document` ' +
+      `WHERE ${Object.entries(params)
+        .filter(([, value]) => value)
+        .map(([key, value]) => `${key}=${key === 'generation' ? value : `'${value}'`}`)
+        .join(' AND ')}`,
+  );
+  return result;
 }
