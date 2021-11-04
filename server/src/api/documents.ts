52219d0ec2f2dc506771a35277933e28bca9fa11
Jeong Yo Han <ingyeoking13@gmail.com>
2021-11-04 10:39:31 +0900

    feat : ranks, 문서 생성 api route 생성
    ranks, 문서 생성 api route 생성

--- server/src/api/documents.ts
+++ server/src/api/documents.ts
@@ -1,10 +1,28 @@
 import * as express from 'express';
-import { getRecentUpdatedDoc } from '../sql/documents-query';
+import { createDoc, getRecentUpdatedDoc, getTopViewedDoc } from '../sql/documents-query';
 
 const router = express.Router();
-router.get('/', async (req: express.Request, res: express.Response) => {
-  let result = await getRecentUpdatedDoc();
-  res.json({ hi: 'boocam' });
+router.get('/recents', async (req: express.Request, res: express.Response) => {
+  const defaultCount = 20;
+  let count = parseInt(req.query.count.toString()) || defaultCount;
+  let result = await getRecentUpdatedDoc({ count: count });
+  res.status(200);
+  res.json({ msg: 'OK', result: result });
+});
+
+router.get('/ranks', async (req: express.Request, res: express.Response) => {
+  const defaultCount = 20;
+  const count = parseInt(req.query.count.toString()) || defaultCount;
+  let result = await getTopViewedDoc({ count: count });
+  res.status(200);
+  res.json({ msg: 'OK', result: result });
+});
+
+router.post('/', async (req: express.Request, res: express.Response) => {
+  console.log(req.body);
+  let result = await createDoc(req.body);
+  res.status(200);
+  res.json({ msg: 'OK', result: result });
 });
 
 export default router;
--- server/src/sql/documents-query.ts
+++ server/src/sql/documents-query.ts
@@ -1,8 +1,15 @@
 import db from '../services/db-pool';
 
-export async function getRecentUpdatedDoc() {
+export async function getRecentUpdatedDoc({ count }: { count: number }) {
   let result = await db.pool.query('SELECT * FROM `update`;');
   return result;
 }
 
-export async function getTopViewedDoc() {}
+export async function getTopViewedDoc({ count }: { count: number }) {
+  let result = await db.pool.query('SELECT * FROM `view`');
+  return result;
+}
+
+export async function createDoc({ count }: { count: number }) {
+  return true;
+}
