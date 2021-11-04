a122bd65613a5952ceae2de81d0f699388ae4660
J045_김웅일 <66904178+wil953742@users.noreply.github.com>
2021-11-04 13:56:33 +0900

    feat: markdown parser & 기본 css 설정
    마크다운 html 변환 후, 기본적인 여백과 구분선  css 설정을 적용했다.

--- client/src/components/main-section/MainSection.jsx
+++ client/src/components/main-section/MainSection.jsx
@@ -1,25 +1,27 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
-import styled from "styled-components";
+import styled from 'styled-components';
 import MainHeader from '../SectionTitle';
+import { md } from './temp';
+import MdParser from '../MdParser';
 
 const Main = styled.div`
-    width: 1115px;
-    height: 1200px;
-    background: white;
-    border: 1px solid #D7D7D7;
-    box-sizing: border-box;
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-    border-radius: 10px;
-    margin-right: 50px;
-    margin-top: 10px;
+  width: 1115px;
+  height: 100%;
+  background: white;
+  border: 1px solid #d7d7d7;
+  box-sizing: border-box;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 10px;
+  margin-right: 50px;
+  margin-top: 10px;
 `;
 
 const MainSection = () => {
-    return (
-        <Main>
-          <MainHeader title='대문' />
-        </Main>
-    );
-}
+  return (
+    <Main>
+      <MainHeader title="대문" />
+      <MdParser content={md} />
+    </Main>
+  );
+};
 export default MainSection;
