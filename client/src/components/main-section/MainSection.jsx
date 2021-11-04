a786736f9bfb29de2f908e1d57604a0cdac46e41
J045_김웅일 <66904178+wil953742@users.noreply.github.com>
2021-11-04 14:43:04 +0900

    feat: loading 컴포넌트 추가
    메인 section에 loading ui 추가

--- client/src/components/main-section/MainSection.jsx
+++ client/src/components/main-section/MainSection.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import MainHeader from '../SectionTitle';
 import { md } from './temp';
+import Loading from '../Loading';
 import MdParser from '../MdParser';
 
 const Main = styled.div`
   width: 1115px;
+  min-height: 1200px;
   height: 100%;
   background: white;
   border: 1px solid #d7d7d7;
@@ -17,10 +19,25 @@ const Main = styled.div`
 `;
 
 const MainSection = () => {
+  const [content, setContent] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getContent = async () => {
+      let res = await fetch('/documents/:대문');
+      res = await res.json();
+      setContent(res);
+      setLoading(false);
+    };
+
+    getContent();
+  }, []);
+
   return (
     <Main>
       <MainHeader title="대문" />
-      <MdParser content={md} />
+      {loading && <Loading />}
+      {!loading && <MdParser content={md} />}
     </Main>
   );
 };
