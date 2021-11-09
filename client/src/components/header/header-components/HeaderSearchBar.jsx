a2f0cfd22459bfaae96a9768bc40ce96653fdb63
suky <0xsuky@gmail.com>
2021-11-09 14:29:36 +0900

    feat: GNB 검색 이벤트 추가
    - GNB 검색 창에 검색 이벤트 추가
    - 현재는 이름이 일치하는 경우만 생성
    - 검색어 입력 후 검색 버튼 클릭시 해당 페이지로 리다이렉트
    - 검색어 입력 후 엔터 누를 시 해당 페이지로 리다이렉트

--- client/src/components/header/header-components/HeaderSearchBar.jsx
+++ client/src/components/header/header-components/HeaderSearchBar.jsx
@@ -1,49 +1,67 @@
-import React from "react";
-import styled from "styled-components";
-import HeaderUser from "./HeaderUser";
+import React, { useRef } from 'react';
+import { useHistory } from 'react-router-dom';
+import styled from 'styled-components';
+import HeaderUser from './HeaderUser';
 import search from '../../../resource/img/search.svg';
 
 const SearchBar = styled.div`
-    display: flex;
-    align-items: center;
+  display: flex;
+  align-items: center;
 `;
 
 const HeaderInput = styled.input`
-    width: 440px;
-    height: 66px;
-    border-top-left-radius: 10px;
-    border-bottom-left-radius: 10px;
-    border: none;
-    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+  width: 440px;
+  height: 66px;
+  border-top-left-radius: 10px;
+  border-bottom-left-radius: 10px;
+  border: none;
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
 `;
 
 const SearchBtn = styled.button`
-    background: white;
-    border: none;
-    height: 66px;
-    cursor: pointer;
-    border-top-right-radius: 10px;
-    border-bottom-right-radius: 10px;
-    filter: drop-shadow(-1px 4px 4px rgba(0, 0, 0, 0.25));
-    margin-right: 30px;
+  background: white;
+  border: none;
+  height: 66px;
+  cursor: pointer;
+  border-top-right-radius: 10px;
+  border-bottom-right-radius: 10px;
+  filter: drop-shadow(-1px 4px 4px rgba(0, 0, 0, 0.25));
+  margin-right: 30px;
 `;
 
 const SearchSVG = styled.img`
-    width: 60px;
-    height: 66px;
-    padding-right: 10px;
-    padding-left: 5px;
-    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+  width: 60px;
+  height: 66px;
+  padding-right: 10px;
+  padding-left: 5px;
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
 `;
 
 const HeaderSearchBar = () => {
-    return(
-        <SearchBar>
-            <HeaderInput type='text' />
-            <SearchBtn><SearchSVG src={search} /></SearchBtn>
-            <HeaderUser />
-        </SearchBar>
-    )
-}
+  const history = useHistory();
+  const searchInput = useRef();
+  const searchBtn = useRef();
+  const submitEvent = (e) => {
+    e.preventDefault();
+    const searchType = 'name';
+    const searchValue = searchInput.current.value;
+    history.push(`/search?${searchType}=${searchValue}`);
+  };
+  const keyPressEvent = (e) => {
+    if (e.key === 'Enter') {
+      searchBtn.current.click();
+    }
+  };
+
+  return (
+    <SearchBar>
+      <HeaderInput type="text" onKeyPress={keyPressEvent} ref={searchInput} />
+      <SearchBtn onClick={submitEvent} ref={searchBtn}>
+        <SearchSVG src={search} />
+      </SearchBtn>
+      <HeaderUser />
+    </SearchBar>
+  );
+};
 
 export default HeaderSearchBar;
