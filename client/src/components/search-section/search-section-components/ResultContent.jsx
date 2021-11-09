3c5d9b5866dcdb1a01e4b04af1ae1eed41e19062
suky <0xsuky@gmail.com>
2021-11-09 20:54:10 +0900

    feat: 검색결과 페이지 content 마크다운 제거
    - 검색결과 페이지 content에 마크다운 문법 제거
    - remark와 strip-markdown 사용

--- client/src/components/search-section/search-section-components/ResultContent.jsx
+++ client/src/components/search-section/search-section-components/ResultContent.jsx
@@ -1,20 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { remark } from 'remark';
+import strip from 'strip-markdown';
 
 const ResultContent = ({ result }) => {
+  const [renderResult, setRenderResult] = useState(result);
+
+  useEffect(async () => {
+    if (!renderResult) {
+      return;
+    }
+    const filteredResult = await Promise.all(
+      renderResult.map(async ({ content, ...props }) => {
+        return {
+          content: await remark()
+            .use(strip)
+            .process(content)
+            .then((file) => file.value),
+          ...props,
+        };
+      }),
+    );
+    setRenderResult(filteredResult);
+  }, []);
+
   return (
     <div>
-      {result?.map(({ generation, boostcamp_id: boostcampId, name, content }) => (
-        <div>
+      {renderResult &&
+        renderResult.map(({ generation, boostcamp_id: boostcampId, name, content }) => (
           <div>
-            <span>{generation}</span>
-            <span> </span>
-            <span>{boostcampId}</span>
-            <span> </span>
-            <span>{name}</span>
+            <div>
+              <span>{generation}</span>
+              <span> </span>
+              <span>{boostcampId}</span>
+              <span> </span>
+              <span>{name}</span>
+            </div>
+            <div>{content}</div>
           </div>
-          <div>{content}</div>
-        </div>
-      ))}
+        ))}
     </div>
   );
 };
