15c19c2962ae5181f677a213e47604b39115ce83
LeeKwangmin <dltkdtn56@naver.com>
2021-11-11 18:11:42 +0900

    style: 섹션 제목 스타일 수정
    margin, padding, width값 수정

--- client/src/components/wiki-section/WikiSection.jsx
+++ client/src/components/wiki-section/WikiSection.jsx
@@ -18,7 +18,7 @@ const Main = styled.div`
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 10px;
   margin-right: 50px;
-  margin-top: 10px;
+  margin-top: 8px;
 `;
 
 const Padd = styled.div`
