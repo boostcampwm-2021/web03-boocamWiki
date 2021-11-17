11a211c1506415194aff3c6ef9e50c63064cdb82
LeeKwangmin <dltkdtn56@naver.com>
2021-11-17 19:01:10 +0900

    feat: 이미지 드래그 앤 드롭 추가 기능 구현
    - 드랍시 오브젝트 스토리지에 추가되는 로직은 구현 완료
    - 받아온 URL을 마크다운 문법으로 변경하여 커서 위치에 추가하는 로직 추가
      필요
    - 드랍시의 커서 위치에 추가하는 방법 찾는 중

--- client/src/components/make-section/make-section-components/Editor.jsx
+++ client/src/components/make-section/make-section-components/Editor.jsx
@@ -1,21 +1,62 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styled from 'styled-components';
 import { font } from '../../../styles/styled-components/mixin';
 
 const Editor = ({ docData, docDispatch }) => {
+  const inputRef = useRef(null);
+
   const changeHandler = (e) => {
     docDispatch({
       type: 'INPUT_DOC_DATA',
       payload: {
-        content: e.target.value
-      }
+        content: e.target.value,
+      },
     });
   };
-  return <EditorBox onChange={changeHandler} value={docData.content} />;
+
+  const tmp = (e) => {
+    // console.log(e);
+    inputRef.current.focus();
+  };
+
+  const dropHandler = async (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+
+    console.log(inputRef.current.value);
+
+    // if (e.dataTransfer.items) {
+    //   const image = e.dataTransfer.items[0].getAsFile();
+    //   const datas = new FormData();
+    //   datas.append('image', image, image.name);
+    //   const result = await fetch('/images', {
+    //     method: 'POST',
+    //     body: datas,
+    //   });
+    //   const url = await result.json();
+    //   console.log(url);
+    //   // 커서 위치에 url 추가하는 로직 필요
+    // }
+
+    // const { selectionStart, selectionEnd } = e.target;
+    // console.log(selectionStart, selectionEnd);
+    // console.log(docData.content.substring(selectionStart, selectionEnd));
+  };
+
+  return (
+    <EditorBox
+      onChange={changeHandler}
+      onDragOver={tmp}
+      onDrop={dropHandler}
+      ref={inputRef}
+      value={docData.content}
+      isDragging
+    />
+  );
 };
 
 const EditorBox = styled.textarea`
-  ${font({size: "16px", weight: "500"})};
+  ${font({ size: '16px', weight: '500' })};
   width: 100%;
   height: 432px;
   resize: none;
