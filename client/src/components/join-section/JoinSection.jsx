86a106297a950699b0a69fcf4aa474cb5a5718b8
suky <0xsuky@gmail.com>
2021-11-23 17:59:20 +0900

    feat: join 페이지 레이아웃 및 로직 변경
    - join 페이지 레이아웃 변경
    - join 시 확인만 클릭하면 되도록 변경

--- client/src/components/join-section/JoinSection.jsx
+++ client/src/components/join-section/JoinSection.jsx
@@ -1,47 +1,21 @@
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
-import { authFetch } from '../../utils/login';
+import React from 'react';
+import styled from 'styled-components';
 import MainSection from '../common/MainSection';
+import Agreement from './join-section-components/Agreement';
+
+const AgreementWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
 
 const JoinSection = () => {
-  const [answer, setAnswer] = useState('');
-  const history = useHistory();
-  const answerChange = (e) => {
-    setAnswer(e.target.value);
-  };
-
-  const answerSubmit = async (e) => {
-    e.preventDefault();
-    const res = await authFetch('/api/auth/join', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ answer }),
-    });
-    const {
-      result: { accessToken, refreshToken },
-      msg,
-    } = await res.json();
-    if (res.status === 200) {
-      window.sessionStorage.setItem('accessToken', accessToken);
-      window.localStorage.setItem('refreshToken', refreshToken);
-      alert('가입이 완료되었습니다.');
-      history.push('/');
-    } else if (msg === 'wrong answer') {
-      alert('잘못된 답을 입력하셨습니다.');
-    }
-
-    setAnswer('');
-  };
-
   return (
     <MainSection title="회원가입">
-      <div>
-        <p>부스트캠프 6기의 시작날짜는?</p>
-        <input name="answer" type="text" value={answer} onChange={answerChange} />
-        <button type="submit" onClick={answerSubmit}>
-          보내기
-        </button>
-      </div>
+      <AgreementWrapper>
+        <Agreement />
+      </AgreementWrapper>
     </MainSection>
   );
 };
---
+++ client/src/components/join-section/join-section-components/AgreementCheckbox.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+import { font } from '../../../styles/styled-components/mixin';
+
+const CheckInput = styled.input`
+  cursor: pointer;
+`;
+
+const CheckSpan = styled.span`
+  ${font({ size: '16px', weight: '500' })}
+  margin: 0px 10px;
+  cursor: pointer;
+`;
+
+const AgreementCheckbox = ({ _ref }) => {
+  const checkClick = (e) => {
+    _ref.current.click();
+  };
+
+  return (
+    <div>
+      <CheckInput type="checkbox" ref={_ref} />
+      <CheckSpan onClick={checkClick}>위 사항을 확인했습니다.</CheckSpan>
+    </div>
+  );
+};
+
+export default AgreementCheckbox;
