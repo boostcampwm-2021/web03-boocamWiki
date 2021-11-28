6d63e269b2362cb05d8bc46e01f0cbfa062834ba
J045_김웅일 <66904178+wil953742@users.noreply.github.com>
2021-11-29 03:41:26 +0900

    feat: MBTI 순위 페이지 구현, 백틱 제거

--- client/src/components/rank-section/rank-components/MbtiRank.jsx
+++ client/src/components/rank-section/rank-components/MbtiRank.jsx
@@ -4,9 +4,10 @@ import { BREAK_POINT_MOBILE } from '../../../utils/display-width';
 import { font, flexBox } from '../../../styles/styled-components/mixin';
 import { MBTIColor } from './style';
 import Loading from '../../common/Loading';
+import MbtiList from './mbti-list-component/MbtiList';
 
 const MbtiRank = () => {
-  const [mbtiCount, setMbtiCount] = useState({});
+  const [mbtiCount, setMbtiCount] = useState({ totalCount: 0, mountedList: [], countList: [] });
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -14,13 +15,20 @@ const MbtiRank = () => {
       const res = await fetch('/api/rank/mbti');
       if (res.status === 200) {
         const { result } = await res.json();
-        setMbtiCount(result);
+        let totalCount = 0;
+        const mountedList = result.map((mbtiCount) => {
+          totalCount += mbtiCount.count;
+          return { mbti: mbtiCount.mbti, count: totalCount };
+        });
+        setMbtiCount({ totalCount, mountedList, countList: result });
         setLoading(false);
       }
     };
 
     getMbtiCount();
   }, []);
+
+  const getReverseEle = (list, idx) => list[list.length - idx - 1];
 
   return (
     <MbtiRankContainer>
@@ -30,11 +38,30 @@ const MbtiRank = () => {
           <MbtiPieChartWrapper>
             <ChartTitle>부캠 MBTI 순위</ChartTitle>
             <MbtiPieChartSVG>
-              <MbtiCircle cx="155" cy="155" r="130" />
+              {mbtiCount.mountedList.map((mbti, idx) => (
+                <MbtiCircle
+                  cx="155"
+                  cy="155"
+                  r="130"
+                  color={MBTIColor[getReverseEle(mbtiCount.mountedList, idx).mbti]}
+                  offset={getReverseEle(mbtiCount.mountedList, idx).count / mbtiCount.totalCount}
+                  key={getReverseEle(mbtiCount.mountedList, idx).mbti}
+                  keyFrame={getReverseEle(mbtiCount.mountedList, idx).mbti}
+                />
+              ))}
             </MbtiPieChartSVG>
           </MbtiPieChartWrapper>
           <MbtiListWrapper>
-            <p>리스트</p>
+            {mbtiCount.countList.map((mbti, idx) => (
+              <MbtiList
+                order={idx + 1}
+                mbti={mbti.mbti}
+                color={MBTIColor[mbti.mbti]}
+                ratio={Math.round((mbti.count * 100) / mbtiCount.totalCount)}
+                count={mbti.count}
+                key={mbti.mbti}
+              />
+            ))}
           </MbtiListWrapper>
         </>
       )}
@@ -49,6 +76,11 @@ const MbtiRankContainer = styled.div`
   border: 1px solid #bbbbbb;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 10px;
+
+  @media only screen and (max-width: ${BREAK_POINT_MOBILE}px) {
+    ${flexBox({ direction: 'column', alignItems: 'center' })};
+    height: 916px;
+  })
 `;
 
 const MbtiPieChartWrapper = styled.div`
@@ -58,14 +90,21 @@ const MbtiPieChartWrapper = styled.div`
 `;
 
 const MbtiListWrapper = styled.div`
-  ${flexBox({ display: 'column', justifyContent: 'center', alignItems: 'center' })}
+  ${flexBox({ direction: 'column', justifyContent: 'flex-start', alignItems: 'center' })}
   width: 355px;
   height: 458px;
   padding: 15px 0;
+  overflow-y: auto;
+
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+  &:: -webkit-scrollbar {
+    display: none;
+  }
 `;
 
 const ChartTitle = styled.p`
-  ${font({ color: '#222222', size: '18px', weight: '500' })};
+  ${font({ color: '#222222', size: '18px', weight: '600' })};
   position: absolute;
   top: 15px;
   left: 15px;
@@ -82,9 +121,20 @@ const MbtiPieChartSVG = styled.svg`
 const MbtiCircle = styled.circle`
   fill: transparent;
   stroke-width: 50;
-  stroke: ${MBTIColor.ENFJ};
-  stroke-dasharray: 50;
-  stroke-dashoffset: 50;
+  stroke: ${(props) => props.color || '#222222'};
+  stroke-dasharray: 817;
+  stroke-dashoffset: 817;
+  transform: rotate(-90deg);
+  transform-origin: 50% 50%;
+  animation: ${(props) => props.keyFrame} 1.5s forwards;
+  @keyframes ${(props) => props.keyFrame} {
+    from {
+      stroke-dashoffset: 817;
+    }
+    to {
+      stroke-dashoffset: ${(props) => (1 - props.offset) * 817};
+    }
+  }
 `;
 
 export default MbtiRank;
